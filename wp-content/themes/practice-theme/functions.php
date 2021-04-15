3bca13e8b58d73104cf0261129745d36695643a3
robinhlgrsn <robinholgersson91@gmail.com>
2021-04-15 14:54:03 +0200

    added widgets to footer

--- wp-content/themes/practice-theme/functions.php
+++ wp-content/themes/practice-theme/functions.php
@@ -1,9 +1,49 @@
 <?php 
   add_theme_support('post-thumbnails');
   add_theme_support('menus');
+  add_theme_support('widgets');
   
   
+
+
+    register_sidebar(
+      array(
+        'name' => 'Footer Info 1',
+        'description' => 'Kort info om sidan i footerfältet',
+        'id' => 'footer',
+        'before_widget' => '<div class="col-xs-12 col-sm-6 col-md-4">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+      )
+    );
+
+
+    register_sidebar(
+      array(
+        'name' => 'Footer Info 2',
+        'description' => 'Kontakt info om sidan i footerfältet',
+        'id' => 'footertwo',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+      )
+    );
+    register_sidebar(
+      array(
+        'name' => 'Footer Social Media',
+        'description' => 'Kontakt info om sidan i footerfältet',
+        'id' => 'footerthree',
+        'before_widget' => '<div class="col-xs-12 col-sm-3 col-md-3 col-md-offset-1">',
+        'after_widget' => '</div>',
+        'before_title' => '<h4>',
+        'after_title' => '</h4>',
+      )
+    );
   
+
+
 
 
   /* Ladda in Css och scripts */
